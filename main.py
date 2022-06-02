fd57bda6c281e2c37b0b64e87e19abe921fa23bd
Aarush Aggarwal <91485103+iam4tart@users.noreply.github.com>
2022-06-02 19:00:22 +0000

    added Flask mobility

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import json
+from flask import Flask, request, jsonify
+
+# instantiating a Flask app
+app = Flask(__name__)
+
+# web scraping lyrics from 'songlyrics.com'
 
 def get_lyrics():
   artist_name = "Rihanna" #input("Artist Name: ")
@@ -12,14 +17,46 @@ def get_lyrics():
 
   soup = BeautifulSoup(response.content,'html5lib')
   data = soup.find('p',attrs={'id':'songLyricsDiv'}).getText()
-  print(data)
-  #format = '{"lyrics":"'+f'{data}'+'"}'
 
-  #format = "{"+f'"lyrics": \"{data}\"'+"}"
-  #jsonobj = json.loads(format)
-  #print(jsonobj)
-get_lyrics()
+  return data
+
+lyrics = get_lyrics()
+
+# routing URLs
+
+@app.route('/lyrics/', methods=['GET'])
+def respond():
+  # retrieve Artist Name from URL parameter
+  # /lyrics/?artist=
+  artist = request.args.get("artist", None)
+  # for debugging
+  print(f'received artist_name: {artist}')
+  # response var
+  response = {}
+
+  # verifying the response variable
+  if not artist:
+    response['ERROR'] = f"No artist found with name: {artist}"
+  elif str(artist).isdigit():
+    response['ERROR'] = "Atleast a million dollar artist"
+  else:
+    response['MESSAGE'] = f"Welcome to Musiva Lyrics API!"
+
+  # return json response
+  return jsonify(response)
+
+#--
+#if __name__ = '__main__':
+  # multiple user access
+  #app.run(threaded=True, port=5000)
 
 
-
-
+  if artist:
+    return jsonify({
+      "lyrics": f"{lyrics}",
+      "METHOD": "POST"
+    })
+  else:
+    return jsonify({
+      "ERROR": f"No artist found with name: {artist}"
+    })
